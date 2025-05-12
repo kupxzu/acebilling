3aabb3c4ec99c97941552dc3bf3e51b02e5d23c8
chicken.penguin <carlcastuel@gmail.com>
2025-05-12 17:37:58 +0800

    update

--- react/src/components/admitting/ViewPatient.jsx
+++ react/src/components/admitting/ViewPatient.jsx
@@ -269,7 +269,7 @@ const ViewPatient = () => {
                                         </svg>
                                         Preview Portal
                                     </a>
-                                    <button
+                                    {/* <button
                                         onClick={generatePortalAccess}
                                         className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700"
                                         disabled={portalData.isLoading}
@@ -286,7 +286,7 @@ const ViewPatient = () => {
                                             </svg>
                                         )}
                                         {portalData.isLoading ? 'Generating...' : 'Regenerate Access'}
-                                    </button>
+                                    </button> */}
                                 </div>
                             </>
                         ) : (
